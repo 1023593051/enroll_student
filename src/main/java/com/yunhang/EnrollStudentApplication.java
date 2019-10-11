188318c64e713c021c6d2c56b91941ba6ea76e20
lz <1023593051@qq.com>
2019-10-11 12:25:17 +0800

    this is a firset commit

---
+++ src/main/java/com/yunhang/EnrollStudentApplication.java
@@ -0,0 +1,13 @@
+package com.yunhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrollStudentApplication.class, args);
+    }
+
+}
